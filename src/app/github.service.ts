13ec5fb0c50f1a43f552d176e47d352d8ce91101
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-05-08 09:04:05 -0400

    r

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,9 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class GithubService {
   url = `https://api.github.com/users/{0}/repos`;
+
   constructor(private http: HttpClient) { }
 
-  public getRepositories(username: string): Observable<an[]> {
+  public getRepositories(username: string): Observable<any[]> {
     const url = `https://api.github.com/users/${username}/repos`;
     console.log(url);
     return this.http.get<any[]>(`https://api.github.com/users/${username}/repos`);
